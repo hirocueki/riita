5c6a6dafc8bec93d6e55d40d71c9364c1aa6a4a0
hirocueki <hirocueki@gmail.com>
2018-04-25 16:06:53 +0900

    command db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425064745) do
+ActiveRecord::Schema.define(version: 20180425070227) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
