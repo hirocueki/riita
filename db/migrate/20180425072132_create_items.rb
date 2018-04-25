74ef86b975b3020c92c58e9fe36d862c515f60e1
hirocueki <hirocueki@gmail.com>
2018-04-25 16:22:44 +0900

    generate scaffold item

--- db/migrate/20180425072132_create_items.rb
+++ db/migrate/20180425072132_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.1]
     create_table :items do |t|
       t.string :title
       t.string :body
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
